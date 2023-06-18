c97ee85280addb4af19421ee267b0de7d762d835
이충재 <knm8643@nate.com>
2023-06-18 17:16:35 +0900

    최초커밋

---
+++ src/main/java/com/example/yurim/YurimApplication.java
@@ -0,0 +1,13 @@
+package com.example.yurim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YurimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YurimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yurim/YurimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yurim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YurimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
